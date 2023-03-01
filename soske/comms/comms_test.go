f51863d5b6cc4675df00fdc98fc4f232b60273db
Stephen Malone <mail@wirehaiku.org>
2023-03-01 19:04:52 +1000

    Added comms functions.

---
+++ soske/comms/comms_test.go
@@ -0,0 +1,18 @@
+package comms
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/wirehaiku/Soske/soske/tools/test"
+)
+
+func TestRun(t *testing.T) {
+	// setup
+	db := test.DB()
+
+	// success
+	outs, err := Run(db, "get", []string{"alpha"})
+	assert.Equal(t, []string{"Alpha two.\n"}, outs)
+	assert.NoError(t, err)
+}
