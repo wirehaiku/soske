9fd1336a46370b9b22cca20017473e465ae95ab2
Stephen Malone <mail@wirehaiku.org>
2023-03-05 07:35:57 +1000

    Added Python package.

---
+++ soske/__init__.py
@@ -0,0 +1,7 @@
+"""
+Package definition for 'soske'.
+"""
+
+VERSION_DATE = "2023-03-01"
+VERSION_NUMS = "0.1.0"
+VERSION_TEXT = f"Soske version {VERSION_NUMS} ({VERSION_DATE})."
---
+++ soske/__main__.py
@@ -0,0 +1,15 @@
+"""
+Main program function.
+"""
+
+
+def main(args: list[str] | None = None):
+    """
+    Run the main Soske program.
+    """
+
+    ...
+
+
+if __name__ == "__main__":
+    main()
